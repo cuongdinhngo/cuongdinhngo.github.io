ec8233cabf1f0e74d0e0f4a7c464692deba516cb
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-18 10:21:49 +0700

    chore: align center

--- app/components/content/About.vue
+++ app/components/content/About.vue
@@ -2,28 +2,30 @@
   <v-row>
     <v-col cols="12" sm="12" md="9" lg="9" class="mx-auto">
       <v-card class="mt-10 elevation-0 bg-transparent" tile width="">
-        <v-list-item>
-          <template #title>
-            <span class="text-h5 font-weight-bold">{{ profile.name }}</span>
-          </template>
-          <template #subtitle>
-            <span class="text-subtitle-1 font-weight-bold">{{ profile.title }}</span>
-          </template>
-          <template #prepend>
-            <v-avatar
-              :size="profile.avatarSize"
-              class="pa-1 bg-white"
-              variant="elevated"
-            >
-              <v-img
-                :src="getImagePath(profile.avatarSrc)"
-                :alt="`${profile.name}'s Avatar`"
-                cover
-                rounded="circle"
-              ></v-img>
-            </v-avatar>
-          </template>
-        </v-list-item>
+        <v-card-title class="d-flex flex-column align-center">
+          <v-list-item>
+            <template #title>
+              <span class="text-h5 font-weight-bold">{{ profile.name }}</span>
+            </template>
+            <template #subtitle>
+              <span class="text-subtitle-1 font-weight-bold">{{ profile.title }}</span>
+            </template>
+            <template #prepend>
+              <v-avatar
+                :size="profile.avatarSize"
+                class="pa-1 bg-white"
+                variant="elevated"
+              >
+                <v-img
+                  :src="getImagePath(profile.avatarSrc)"
+                  :alt="`${profile.name}'s Avatar`"
+                  cover
+                  rounded="circle"
+                ></v-img>
+              </v-avatar>
+            </template>
+          </v-list-item>
+        </v-card-title>
 
         <v-card-title class="text-center text-h5 font-weight-bold text-wrap">
           <slot name="hero_title" />
